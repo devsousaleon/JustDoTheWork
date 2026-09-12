510fe007ee4c531f8c1ff192cfe4f37aa4d04ae2
Leonardo Sousa <sousaleon1508@gmail.com>
2026-09-12 17:11:00 -0300

    1.5.0 -> Implementa Menu Lateral
    Implementado menu lateral no sistema. Permanece tela de execuções como principal e as demais rotinas serão abertas como "abas"

--- JustDoTheWork.Sistema/Forms/FormPrincipal.Designer.cs
+++ JustDoTheWork.Sistema/Forms/FormPrincipal.Designer.cs
@@ -29,55 +29,147 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPrincipal));
-            XtratabControl = new DevExpress.XtraTab.XtraTabControl();
+            xtraTabControlMain = new DevExpress.XtraTab.XtraTabControl();
             tabTarefas = new DevExpress.XtraTab.XtraTabPage();
-            tabCadastro = new DevExpress.XtraTab.XtraTabPage();
-            tabHistorico = new DevExpress.XtraTab.XtraTabPage();
-            tabCadastroModeloRelatorio = new DevExpress.XtraTab.XtraTabPage();
-            ((System.ComponentModel.ISupportInitialize)XtratabControl).BeginInit();
-            XtratabControl.SuspendLayout();
+            coltreeListColumns = new DevExpress.XtraTreeList.Columns.TreeListColumn();
+            accordionControlMenu = new DevExpress.XtraBars.Navigation.AccordionControl();
+            accordionControlElementCadastro = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlSeparator1 = new DevExpress.XtraBars.Navigation.AccordionControlSeparator();
+            ElementAtividades = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            ElementModeloRelatorio = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElementRelatorio = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlSeparator2 = new DevExpress.XtraBars.Navigation.AccordionControlSeparator();
+            ElementRelatorioExecucao = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            accordionControlElement1 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            ((System.ComponentModel.ISupportInitialize)xtraTabControlMain).BeginInit();
+            xtraTabControlMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)accordionControlMenu).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)panelControl1).BeginInit();
+            panelControl1.SuspendLayout();
             SuspendLayout();
             // 
-            // XtratabControl
+            // xtraTabControlMain
             // 
-            XtratabControl.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            XtratabControl.Location = new Point(12, 12);
-            XtratabControl.Name = "XtratabControl";
-            XtratabControl.SelectedTabPage = tabTarefas;
-            XtratabControl.Size = new Size(1254, 664);
-            XtratabControl.TabIndex = 0;
-            XtratabControl.TabPages.AddRange(new DevExpress.XtraTab.XtraTabPage[] { tabTarefas, tabCadastro, tabHistorico, tabCadastroModeloRelatorio });
+            xtraTabControlMain.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            xtraTabControlMain.ClosePageButtonShowMode = DevExpress.XtraTab.ClosePageButtonShowMode.InAllTabPageHeaders;
+            xtraTabControlMain.Location = new Point(60, 9);
+            xtraTabControlMain.Name = "xtraTabControlMain";
+            xtraTabControlMain.SelectedTabPage = tabTarefas;
+            xtraTabControlMain.ShowTabHeader = DevExpress.Utils.DefaultBoolean.True;
+            xtraTabControlMain.Size = new Size(1188, 659);
+            xtraTabControlMain.TabIndex = 0;
+            xtraTabControlMain.TabPages.AddRange(new DevExpress.XtraTab.XtraTabPage[] { tabTarefas });
+            xtraTabControlMain.CloseButtonClick += xtraTabControlMain_CloseButtonClick;
             // 
             // tabTarefas
             // 
             tabTarefas.Name = "tabTarefas";
-            tabTarefas.Size = new Size(1252, 633);
+            tabTarefas.Size = new Size(1186, 628);
             tabTarefas.Text = "Execução de Tarefas";
             // 
-            // tabCadastro
+            // coltreeListColumns
             // 
-            tabCadastro.Name = "tabCadastro";
-            tabCadastro.Size = new Size(1252, 639);
-            tabCadastro.Text = "Cadastro de Tarefas";
+            coltreeListColumns.Caption = "Rotinas";
+            coltreeListColumns.FieldName = "Rotinas";
+            coltreeListColumns.Name = "coltreeListColumns";
+            coltreeListColumns.Visible = true;
+            coltreeListColumns.VisibleIndex = 0;
             // 
-            // tabHistorico
+            // accordionControlMenu
             // 
-            tabHistorico.Name = "tabHistorico";
-            tabHistorico.Size = new Size(1252, 639);
-            tabHistorico.Text = "Histórico de execuções";
+            accordionControlMenu.AnimationType = DevExpress.XtraBars.Navigation.AnimationType.Spline;
+            accordionControlMenu.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] { accordionControlElementCadastro, accordionControlElementRelatorio });
+            accordionControlMenu.Location = new Point(5, 9);
+            accordionControlMenu.Name = "accordionControlMenu";
+            accordionControlMenu.OptionsHamburgerMenu.DisplayMode = DevExpress.XtraBars.Navigation.AccordionControlDisplayMode.Minimal;
+            accordionControlMenu.OptionsMinimizing.AllowMinimizeMode = DevExpress.Utils.DefaultBoolean.True;
+            accordionControlMenu.OptionsMinimizing.State = DevExpress.XtraBars.Navigation.AccordionControlState.Minimized;
+            accordionControlMenu.RootDisplayMode = DevExpress.XtraBars.Navigation.AccordionControlRootDisplayMode.Footer;
+            accordionControlMenu.ScrollBarMode = DevExpress.XtraBars.Navigation.ScrollBarMode.Auto;
+            accordionControlMenu.ShowFilterControl = DevExpress.XtraBars.Navigation.ShowFilterControl.Always;
+            accordionControlMenu.Size = new Size(49, 658);
+            accordionControlMenu.TabIndex = 1;
+            accordionControlMenu.ViewType = DevExpress.XtraBars.Navigation.AccordionControlViewType.HamburgerMenu;
             // 
-            // tabCadastroModeloRelatorio
+            // accordionControlElementCadastro
             // 
-            tabCadastroModeloRelatorio.Name = "tabCadastroModeloRelatorio";
-            tabCadastroModeloRelatorio.Size = new Size(0, 0);
-            tabCadastroModeloRelatorio.Text = "Modelos de Relatório";
+            accordionControlElementCadastro.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] { accordionControlSeparator1, ElementAtividades, ElementModeloRelatorio });
+            accordionControlElementCadastro.Expanded = true;
+            accordionControlElementCadastro.Hint = "Cadastros";
+            accordionControlElementCadastro.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("accordionControlElementCadastro.ImageOptions.SvgImage");
+            accordionControlElementCadastro.Name = "accordionControlElementCadastro";
+            accordionControlElementCadastro.Text = "Cadastro";
+            // 
+            // accordionControlSeparator1
+            // 
+            accordionControlSeparator1.Name = "accordionControlSeparator1";
+            // 
+            // ElementAtividades
+            // 
+            ElementAtividades.Hint = "Cadastro de Atividades";
+            ElementAtividades.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("ElementAtividades.ImageOptions.SvgImage");
+            ElementAtividades.Name = "ElementAtividades";
+            ElementAtividades.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            ElementAtividades.Tag = "Atividades";
+            ElementAtividades.Text = "Atividades";
+            ElementAtividades.Click += AbreNovaJanela_Click;
+            // 
+            // ElementModeloRelatorio
+            // 
+            ElementModeloRelatorio.Hint = "Cadastrar Modelos para Relatórios";
+            ElementModeloRelatorio.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("ElementModeloRelatorio.ImageOptions.SvgImage");
+            ElementModeloRelatorio.Name = "ElementModeloRelatorio";
+            ElementModeloRelatorio.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            ElementModeloRelatorio.Tag = "ModelosRelatorio";
+            ElementModeloRelatorio.Text = "Modelos de Relatório";
+            ElementModeloRelatorio.Click += AbreNovaJanela_Click;
+            // 
+            // accordionControlElementRelatorio
+            // 
+            accordionControlElementRelatorio.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] { accordionControlSeparator2, ElementRelatorioExecucao });
+            accordionControlElementRelatorio.Expanded = true;
+            accordionControlElementRelatorio.Hint = "Relatórios";
+            accordionControlElementRelatorio.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("accordionControlElementRelatorio.ImageOptions.SvgImage");
+            accordionControlElementRelatorio.Name = "accordionControlElementRelatorio";
+            accordionControlElementRelatorio.Text = "Relatorio";
+            // 
+            // accordionControlSeparator2
+            // 
+            accordionControlSeparator2.Name = "accordionControlSeparator2";
+            // 
+            // ElementRelatorioExecucao
+            // 
+            ElementRelatorioExecucao.Hint = "Relatório de execuções de tarefas";
+            ElementRelatorioExecucao.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("ElementRelatorioExecucao.ImageOptions.SvgImage");
+            ElementRelatorioExecucao.Name = "ElementRelatorioExecucao";
+            ElementRelatorioExecucao.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            ElementRelatorioExecucao.Tag = "RelatorioExecucao";
+            ElementRelatorioExecucao.Text = "Relatório de Execuções";
+            ElementRelatorioExecucao.Click += AbreNovaJanela_Click;
+            // 
+            // panelControl1
+            // 
+            panelControl1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panelControl1.Controls.Add(accordionControlMenu);
+            panelControl1.Controls.Add(xtraTabControlMain);
+            panelControl1.Location = new Point(12, 3);
+            panelControl1.Name = "panelControl1";
+            panelControl1.Size = new Size(1253, 673);
+            panelControl1.TabIndex = 2;
+            // 
+            // accordionControlElement1
+            // 
+            accordionControlElement1.Expanded = true;
+            accordionControlElement1.Name = "accordionControlElement1";
+            accordionControlElement1.Text = "Element1";
             // 
             // FormPrincipal
             // 
             AutoScaleDimensions = new SizeF(6F, 13F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1278, 688);
-            Controls.Add(XtratabControl);
+            Controls.Add(panelControl1);
             IconOptions.LargeImage = (Image)resources.GetObject("FormPrincipal.IconOptions.LargeImage");
             MinimumSize = new Size(1280, 722);
             Name = "FormPrincipal";
@@ -85,17 +177,28 @@
             Text = "Home";
             WindowState = FormWindowState.Maximized;
             Load += FormPrincipal_Load;
-            ((System.ComponentModel.ISupportInitialize)XtratabControl).EndInit();
-            XtratabControl.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)xtraTabControlMain).EndInit();
+            xtraTabControlMain.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)accordionControlMenu).EndInit();
+            ((System.ComponentModel.ISupportInitialize)panelControl1).EndInit();
+            panelControl1.ResumeLayout(false);
             ResumeLayout(false);
 
         }
 
         #endregion
-        private DevExpress.XtraTab.XtraTabControl XtratabControl;
+        private DevExpress.XtraTab.XtraTabControl xtraTabControlMain;
         private DevExpress.XtraTab.XtraTabPage tabTarefas;
-        private DevExpress.XtraTab.XtraTabPage tabCadastro;
-        private DevExpress.XtraTab.XtraTabPage tabHistorico;
-        private DevExpress.XtraTab.XtraTabPage tabCadastroModeloRelatorio;
+        private DevExpress.XtraTreeList.Columns.TreeListColumn coltreeListColumns;
+        private DevExpress.XtraBars.Navigation.AccordionControl accordionControlMenu;
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement1;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElementCadastro;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement ElementAtividades;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement ElementModeloRelatorio;
+        private DevExpress.XtraBars.Navigation.AccordionControlSeparator accordionControlSeparator1;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElementRelatorio;
+        private DevExpress.XtraBars.Navigation.AccordionControlSeparator accordionControlSeparator2;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement ElementRelatorioExecucao;
     }
 }
--- JustDoTheWork.Sistema/Forms/FormPrincipal.cs
+++ JustDoTheWork.Sistema/Forms/FormPrincipal.cs
@@ -1,5 +1,8 @@
-﻿using DevExpress.XtraEditors;
+﻿using DevExpress.Utils;
+using DevExpress.XtraBars.Navigation;
+using DevExpress.XtraEditors;
 using DevExpress.XtraTab;
+using DevExpress.XtraTab.ViewInfo;
 using JustDoTheWork.Sistema.ControlPanel;
 
 namespace JustDoTheWork.Sistema.Forms
@@ -13,17 +16,66 @@ namespace JustDoTheWork.Sistema.Forms
 
         void FormPrincipal_Load(object sender, EventArgs e)
         {
-            CarregaDadosTabControl(tabTarefas, new VisualizaExecucaoUserControl());
-            CarregaDadosTabControl(tabCadastro, new VisualizaAtividadesUserControl());
-            CarregaDadosTabControl(tabHistorico, new HistoricoUserControl());
-            CarregaDadosTabControl(tabCadastroModeloRelatorio, new CadastroModeloRelatorioUserControl());
+            CarregaTelaPrincipalExecucao(new VisualizaExecucaoUserControl());
         }
 
-        void CarregaDadosTabControl(XtraTabPage aba, XtraUserControl control)
+        void CarregaTelaPrincipalExecucao(XtraUserControl xtraUserControl)
         {
-            aba.Controls.Clear();
-            control.Dock = DockStyle.Fill;
-            aba.Controls.Add(control);
+            tabTarefas.Controls.Clear();
+            tabTarefas.Controls.Add(xtraUserControl);
+            xtraUserControl.Dock = DockStyle.Fill;
+
+            tabTarefas.ShowCloseButton = DefaultBoolean.False;
+        }
+
+        void CarregaDadosTabControl(XtraTabControl xtratabControlMain, XtraUserControl xtraUserControl, string nomeTab)
+        {
+            foreach (XtraTabPage aba in xtraTabControlMain.TabPages)
+            {
+                if (string.Equals(aba.Text, nomeTab, StringComparison.OrdinalIgnoreCase))
+                {
+                    xtraUserControl.Dispose();
+                    return;
+                }
+            }
+
+            XtraTabPage novaAba = xtratabControlMain.TabPages.Add();
+
+            novaAba.Controls.Clear();
+            xtraUserControl.Dock = DockStyle.Fill;
+            novaAba.Controls.Add(xtraUserControl);
+            novaAba.Text = nomeTab;
+            xtraTabControlMain.SelectedTabPage = novaAba;
+
+            accordionControlMenu.OptionsMinimizing.State = AccordionControlState.Minimized;
+        }
+
+        void AbreNovaJanela_Click(object sender, EventArgs e)
+        {
+            if (sender is not AccordionControlElement elemento)
+                return;
+
+            switch (elemento.Tag)
+            {
+                case "Atividades": CarregaDadosTabControl(xtraTabControlMain, new VisualizaAtividadesUserControl(), "Cadastro de Atividades"); return;
+                case "ModelosRelatorio": CarregaDadosTabControl(xtraTabControlMain, new CadastroModeloRelatorioUserControl(), "Modelos de Relatório"); return;
+                case "RelatorioExecucao": CarregaDadosTabControl(xtraTabControlMain, new HistoricoUserControl(), "Execuções realizadas"); return;
+            }
+        }
+
+        private void xtraTabControlMain_CloseButtonClick(object sender, EventArgs e)
+        {
+            if (e is not ClosePageButtonEventArgs argumentos)
+                return;
+
+            if (argumentos.Page is not XtraTabPage aba)
+                return;
+
+            if (aba == tabTarefas)
+                return;
+
+            xtraTabControlMain.TabPages.Remove(aba);
+            aba.Dispose();
         }
     }
 }
